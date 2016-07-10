6b4352d7a710825ec8208d2126105f59e776c3ee
Daniel Huang <danielhuang@g.ucla.edu>
2016-07-09 18:47:13 -0700

    Arch complete-ish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-
+    var start = new Date(2016,6,10),
+        end = new Date(2016,8,19),
+        today = new Date();
+    alert( Math.round((end - start) * 100) / today );
 
 });
