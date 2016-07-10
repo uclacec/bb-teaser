c17d6f6609a4f8787e9914829f4b1d89b03d53b5
Daniel Huang <danielhuang@g.ucla.edu>
2016-07-09 19:07:33 -0700

    positioned neon arch, on click turns dark

---
+++ js/glow.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Daniel on 7/9/2016.
+ */
--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
     var start = new Date(2016,6,10),
         end = new Date(2016,8,19),
         today = new Date();
-    alert( Math.round((end - start) * 100) / today );
+    //alert( Math.round((end - start) * 100) / today );
 
+    //when clicked, background changes to blue, and royce becomes neon
+    $(window).on('click',function(){
+        $('body').css({'background-color':'darkblue'});
+
+    });
 });
